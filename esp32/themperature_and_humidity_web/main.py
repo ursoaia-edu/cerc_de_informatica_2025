8db24ecedd16f2201ff31f36a839886e10981ab3
Nicolae Postica <nicolae.postica2@gmail.com>
2026-04-10 00:38:07 +0300

    Refactor connection handling in main.py: implement timeout for WiFi connection and improve readability

--- esp32/themperature_and_humidity_web/main.py
+++ esp32/themperature_and_humidity_web/main.py
@@ -19,17 +19,30 @@ ssid = 'REPLACE_WITH_YOUR_SSID'
 password = 'REPLACE_WITH_YOUR_PASSWORD'
 
 station = network.WLAN(network.STA_IF)
+station.active(False)
 station.active(True)
+station.disconnect()
 station.connect(ssid, password)
 
-while station.isconnected() == False:
-  pass
+import time
+max_wait = 20
+while max_wait > 0:
+  if station.isconnected():
+    break
+  max_wait -= 1
+  print('waiting for connection...')
+  time.sleep(1)
 
-print('Connection successful')
-print(station.ifconfig())
+if station.isconnected():
+  print('Connection successful')
+  print(station.ifconfig())
+else:
+  print('Connection failed')
+  import sys
+  sys.exit()
 
-sensor = dht.DHT22(Pin(14))
-#sensor = dht.DHT11(Pin(14))
+sensor = dht.DHT11(Pin(4))
+#sensor = dht.DHT22(Pin(4))
 
 def read_sensor():
   global temp, hum
